ec74a5c3fa30110f2adf58d8a9f18aad9fa70d15
LOKESH KUMAR <lokeshkumarbijnor@gmail.com>
2020-06-07 17:08:22 +0530

    f

---
+++ precache-manifest.f36d867bbf62a90d18c373c8b00a5ac2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff849b0850abdbf185f634ec3da0a6f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e199e278b01d1570d1f",
+    "url": "/static/css/main.19735c56.chunk.css"
+  },
+  {
+    "revision": "fe6e1479063057526296",
+    "url": "/static/js/2.4fc7d2a1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.4fc7d2a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e199e278b01d1570d1f",
+    "url": "/static/js/main.63266cfb.chunk.js"
+  },
+  {
+    "revision": "69d2b7e1484e517270ab",
+    "url": "/static/js/runtime-main.788e1730.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
